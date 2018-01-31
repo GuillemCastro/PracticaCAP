3f9292cb932b41cc12e1552d4b4e34eeaf94c166
David <david17sego@gmail.com>
2018-01-31 16:13:50 +0100

    Added test for callcc

--- src/callcc.js
+++ src/callcc.js
@@ -1,4 +1,19 @@
 function callcc(f) {
-	var k = Continuation();
+	var k = new Continuation();
 	return f(k);
+}
+
+var cont;
+function test(a) {
+	cont = a;
+	return 'First execution';
+}
+
+var newTest = callcc(test);
+if (newTest === 'First execution') {
+	cont(1);
+} else if (newTest === 1) {
+	print('El resultat es correcte.');
+} else {
+	print('El call/cc no funciona')
 }
